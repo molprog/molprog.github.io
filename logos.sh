95adc5c2dbca8d7cfb8f71152547771e642a571c
William Earley <william@earley.org.uk>
2021-06-24 18:51:50 +0100

    logos + email stylesheet

--- logos.sh
+++ logos.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd assets
 export TMPDIR="$(mktemp -d)"
+export TMPSVG="${TMPDIR}/logo-white.svg"
 
-svgexport logo-white.svg logo-white.png 10x
-svgexport logo-white.svg "${TMPDIR}/fav-white.png" 0:-10:88:120 980:980 pad
+sed 's|</svg>|<defs><style>@import url("https://molecularprogrammers.org/assets/font/univers/style.css");</style></defs></svg>|' logo-white.svg > "${TMPSVG}"
+svgexport "${TMPSVG}" logo-white.png 10x
+svgexport "${TMPSVG}" banner-800.png :1600
+svgexport "${TMPSVG}" "${TMPDIR}/fav-white.png" 0:-10:88:120 980:980 pad
+svgexport "${TMPSVG}" logo-300.png 0:-10:88:120 600:600 pad
 
 (cd "$TMPDIR"
   convert "fav-white.png" -background "#121416" -flatten "favicon.png"
